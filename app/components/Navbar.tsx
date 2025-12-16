20da8e4eeb50f42da5033e7b2bc202bedb50c290
Saves <martin.l74123@gmail.com>
2025-12-17 00:22:36 +0100

    Navbar styling

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -36,26 +36,28 @@ function Navbar({ name, webLink }: Props) {
     }, [isBurgerOpen]);
 
   return (
-    <section className="flex justify-between items-center p-4 bg-gray-800 text-white relative">
-        <div className="text-3xl">{name}</div>
+    <section className="flex justify-between items-center p-4 bg-gray-800 text-white relative h-25 lg:px-15">
+        <div className="text-3xl lg:text-4xl font-semibold lg:w-1/2">{name}</div>
 
-    
-        <div>
+     {/* Burger Menu */}
+        <div className="md:flex md:w-1/2">
             <button onClick={toggleBurger} ref={buttonRef} className="flex flex-col w-10 space-y-2 cursor-pointer md:hidden">
                 <span className="h-1 bg-white w-full rounded-full"></span>
                 <span className="h-1 bg-white w-full rounded-full"></span>
                 <span className="h-1 bg-white w-full rounded-full"></span>
             </button>
 
-            <ul className="hidden md:flex space-x-4">
+    {/* Normal Menu */}
+            <ul className="hidden md:flex space-x-4 md:gap-5 lg:gap-10 justify-center w-full">
         {webLink.map((link, index) => (
-          <li key={index}>
+          <li key={index} className="text-2xl lg:text-3xl font-light border-b-2 border-transparent py-2 hover:border-gray-400 hover:text-gray-300 transition-all duration-300">
             <a href={`#${link.description}`}>{link.title}</a>
           </li>
         ))}
         </ul>
         </div>
 
+    {/* Opened Burger Menu */}
         {isBurgerOpen && (
         <div
           id="mobile-menu"
